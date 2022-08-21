7f5f1f37a6ef55a6123bb208d43bb4c9b84dc5ba
Joe Hernandez <joe.hernandez@instructure.com>
2022-08-21 18:33:40 -0500

    Completed #47

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { Component } from 'react';
 
+import CardList from './components/card-list/card-list.component';
 import './App.css';
 
 class App extends Component {
@@ -7,50 +8,45 @@ class App extends Component {
     super()
 
     this.state = {
-      monsters: []
+      monsters: [],
+      searchField: ''
     }
-    console.log('constructor')
   }
 
   componentDidMount() {
-    console.log('componentDidMount')
     fetch('https://jsonplaceholder.typicode.com/users')
       .then(response => response.json())
       .then((users) => this.setState(() => {
         return {monsters: users}
-      },
-      () => {
-        console.log(this.state)
-      }))
+      })
+    )
   }
 
   render() {
-    console.log('render')
+    const { monsters, searchField } = this.state
+    const { onSearchChange } = this
+
+    const filteredMonsters = monsters.filter((monster) => {
+      return monster.name.toLocaleLowerCase().includes(searchField)
+    })
+
     return (
       <div className="App">
         <input 
           className='search-box' 
           type='search' 
           placeholder='search monsters'
-          onChange={(event) => this.onSearchChange(event)}
+          onChange={onSearchChange}
         />
-        {this.state.monsters.map((monster) => {
-          return (
-            <div key={monster.id}>
-              <h1>{monster.name}</h1>
-            </div>
-          )
-        })}
+        <CardList monsters={filteredMonsters}/>
       </div>
     );
   }
 
-  onSearchChange(event) {
-    const searchString = event.target.value.toLocaleLowerCase()
-    const filteredMonsters = this.state.monsters.filter((monster) => {
-      return monster.name.toLocaleLowerCase().includes(searchString)
-    })
-    this.setState(() => {return {monsters: filteredMonsters}})
+  onSearchChange = (event) => {
+    const searchField = event.target.value.toLocaleLowerCase()
+    
+    this.setState(() => {return { searchField }})
   }
 }
 
